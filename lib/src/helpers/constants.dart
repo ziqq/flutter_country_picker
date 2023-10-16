6217a96af84fcffbd0bbc5d05732c5fba5e62a30
Anton Ustinoff <a.a.ustinoff@gmail.com>
2023-10-16 11:26:57 +0400

    feat(3.1.0): added `typesdef` & `constants`

---
+++ lib/src/helpers/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const double kDefaultIndent = 10.0;
+const double kDefaultPadding = 16.0;
+
+const Radius kDefaultBorderRadius = Radius.circular(10);
+const TextStyle kDefaultTextStyle = TextStyle(fontSize: 16);
+
+const String kDefaultPhoneMask = '000 000 0000';
